148890d706115438ca83d80678bd886519707915
haakensonb <untamedhawk44@gmail.com>
2025-04-25 20:09:11 -0700

    Remove unneeded config from context

--- Backend/Models/BackendContext.cs
+++ Backend/Models/BackendContext.cs
@@ -12,13 +12,4 @@ public class BackendContext : DbContext
     public DbSet<Board> Boards { get; set; } = null!;
     public DbSet<BoardList> BoardLists { get; set; } = null!;
     public DbSet<Card> Cards { get; set; } = null!;
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<BoardList>()
-            .HasMany(e => e.Cards)
-            .WithOne(e => e.BoardList)
-            .HasForeignKey(e => e.BoardListId)
-            .IsRequired();
-    }
 }
